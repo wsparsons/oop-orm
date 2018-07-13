fb2609ff71d5f7827660809c8fe76a3d3703589f
Wes Reid <bwreid@gmail.com>
2018-07-13 11:25:45 -0700

    Install knex, pg, dotenv with scripts

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+require('dotenv').load()
+const path = require('path')
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, 'src', 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'src', 'db', 'seeds')
+    }
+  }
+}
